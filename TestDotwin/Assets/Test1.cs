8e91be6c1b78445c1f4591880db127828b9bbf2e
timurg <timurg@playtika.com>
2020-10-16 13:25:28 +0300

    1 and 2 test Reload

---
+++ TestDotwin/Assets/Test1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    [SerializeField] private Transform ButtomUp;
+    [SerializeField] private Transform ButtomDonw;
+    
+    
+    private void Up()
+    {
+        var tagertPosition= transform.up * 4;
+        var durat = 2f;
+        var sec = DOTween.Sequence();
+        sec.Append(transform.DOMove(tagertPosition, durat));
+    }
+
+    private void Down()
+    { 
+        var tagertPosition= -transform.up * 4;
+        var durat = 2f; 
+        var sec = DOTween.Sequence();
+       sec.Append(transform.DOMove(tagertPosition, durat));
+    }
+}
