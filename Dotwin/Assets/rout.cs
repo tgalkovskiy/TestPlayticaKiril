2c0e5518c8ed971c63dfe15c02c1e42818e5f017
timurg <timurg@playtika.com>
2020-10-16 12:42:58 +0300

    1 test

---
+++ Dotwin/Assets/rout.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rout : MonoBehaviour
+{
+    [SerializeField] private Transform tagert;
+    [SerializeField] private Transform[] points;
+    [SerializeField] private Button PlayBat;
+    [SerializeField] private Button StopPlay;
+
+    private Tweener _tweener;
+
+    private void Start()
+    {
+        PlayBat.onClick.AddListener(Play);
+        StopPlay.onClick.AddListener(Stop);
+    }
+
+    // Start is called before the first frame update
+    public void Play()
+    {
+        var path = new Vector3[points.Length];
+        for (int i = 0; i < points.Length; i++)
+        {
+            path[i] = points[i].position;
+        }
+
+        tagert.DOPath(path, 5f);
+    }
+
+    private void Stop()
+    {
+        Debug.Log("!");
+        _tweener.Pause();
+       
+    }
+}
---
+++ Dotwin/Assets/test1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class test1 : MonoBehaviour
+{
+    [SerializeField] private Transform ButtomUp;
+
+    [SerializeField] private Transform ButtomDown;
+
+    [SerializeField] private Button _Up;
+    [SerializeField] private Button _Down;
+
+    private float tagertposY = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Up.onClick.AddListener(Up);
+        _Down.onClick.AddListener(Dow);
+    }
+
+    private void Up()
+    {
+        ButtomUp.DOMoveY(tagertposY, 1f);
+    }
+
+    private void Dow()
+    {
+        ButtomDown.DOMoveX(tagertposY, 1f);
+    }
+    
+}
